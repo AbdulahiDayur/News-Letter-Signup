8ae48dc98d6e1d4ecde9f36001176be3060119f1
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-02-13 18:05:13 -0800

    .

--- app.js
+++ app.js
@@ -17,7 +17,44 @@ app.post("/", (req, res) => {
   let lastName = req.body.lastName;
   let emailName = req.body.emailName;
 
-  console.log(`First Name: ${firstName} Last Name: ${lastName} Email: ${emailName}`);
+  const data = {
+    member: [{
+      email_address: emailName,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/f833c63051";
+
+  const options = {
+    method: "POST",
+    auth: "aDayur1:e4694dafc20fc12e1cc1acbb10d48a57-us14"
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.send("Successfully subscribed");
+    } else {
+      res.sendFile(path.join(__dirname + "/failer.html"));
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failer", (req, res) => {
+  res.redirect('/')
 })
 
 app.listen(port, () => {
